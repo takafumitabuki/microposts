f340e88ad9954482b091ff8daff6aef7a45b7ebe
takafumitabuki <takafumi.tabuki@gmail.com>
2016-09-19 10:41:12 +0000

    add region5

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919103234) do
+ActiveRecord::Schema.define(version: 20160919103951) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "region"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
@@ -30,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160919103234) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "region"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
